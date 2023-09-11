856aef49dad29c8e2bed37f61f3fe29579d92f86
Hidy97 <csaladi.alexandra@gmail.com>
2023-09-11 11:02:27 +0200

    Első switch alapok, hibás

--- src/gondolatolvaso2/GondolatOlvaso2.java
+++ src/gondolatolvaso2/GondolatOlvaso2.java
@@ -3,27 +3,34 @@ package gondolatolvaso2;
 import java.util.Scanner;
 
 public class GondolatOlvaso2 {
+
     private static String[] pakli = new String[21];
 
     public static void main(String[] args) {
         Kirak();
+        Megjelenit();
         Melyik();
         Kever();
         EzVolt();
     }
-    
+
     private static String[] Kirak() {
         String[] szinek = {"P", "T", "Z", "M"};
         String[] ertekek = {"Ász", "Kir", "Fel", "X", "IX", "VIII"};
-        
-            int index = 0;          
-            for (int szin = 0; szin < szinek.length; szin++) {
-                for (int ertek = 0; ertek < ertekek.length-1; ertek++) {
-                        pakli[index]= szinek[szin]+"_"+ertekek[ertek];
-                        index++;
-                }
+
+        int index = 0;
+        for (int szin = 0; szin < szinek.length; szin++) {
+            for (int ertek = 0; ertek < ertekek.length - 1; ertek++) {
+                pakli[index] = szinek[szin] + "_" + ertekek[ertek];
+                index++;
             }
-            
+        }
+
+        
+        return pakli;
+    }
+    
+    private static void Megjelenit() {
         for (int szamlalo = 0; szamlalo < pakli.length; szamlalo++) {
             if (szamlalo % 3 == 0) {
                 System.out.println(" ");
@@ -31,8 +38,7 @@ public class GondolatOlvaso2 {
             } else {
                 System.out.printf("%-8s", pakli[szamlalo]);
             }
-        }          
-        return pakli;
+        }
     }
 
     private static void Melyik() {
@@ -40,22 +46,40 @@ public class GondolatOlvaso2 {
         System.out.println("Kérem adja meg melyik oszlopot választja:");
         Scanner sc = new Scanner(System.in);
         boolean jo;
-        
+        /*
         do {
             System.out.println("oszlop(1-3): ");
             int oszlop = sc.nextInt();
             jo = oszlop>=1 && oszlop <= 3;
         } while (!jo);
-        
+         */
+
     }
 
     private static void Kever() {
+        int oszlop = 1;
+        String csereHely = "";
+        switch (1) {
+            case 1:
+                for (int i = 1; i <= 7; i++) {
+                    //keplet (pakli.length-1)-(i-1)*3
+                    csereHely = pakli[i];
+                    int pakliIndex = (pakli.length-1)-(i-1)*3;
+                    pakli[i] = pakli[pakliIndex];
+                }
+                break;
+            default:
+                throw new AssertionError();
+        }
         
+        Megjelenit();
     }
 
     private static boolean EzVolt() {
         boolean ezVolt = false;
         return ezVolt;
     }
+
     
+
 }
